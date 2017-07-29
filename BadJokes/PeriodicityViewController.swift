84d862f213c6e550f8e0f744cd9cdcf0348f5671
Horváth Balázs <balazs630@icloud.com>
2017-07-30 01:05:54 +0200

    Add basic implementation of tableviewcontrollers, add more jokes to the text file and nav bar icon for settings

--- BadJokes/PeriodicityViewController.swift
+++ BadJokes/PeriodicityViewController.swift
@@ -10,4 +10,26 @@ import UIKit
 
 class PeriodicityViewController: UITableViewController {
 
+    var lastSelectedIndexPath = IndexPath(item: 0, section: 0)
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        tableView.selectRow(at: lastSelectedIndexPath, animated: true, scrollPosition: UITableViewScrollPosition.none)
+        tableView.cellForRow(at: lastSelectedIndexPath)?.accessoryType = .checkmark
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = .checkmark
+            lastSelectedIndexPath = indexPath
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = .none
+        }
+    }
+
 }
--- BadJokes/TimeViewController.swift
+++ BadJokes/TimeViewController.swift
@@ -10,4 +10,44 @@ import UIKit
 
 class TimeViewController: UITableViewController {
 
+    @IBOutlet weak var timePicker: UIDatePicker!
+
+    var lastSelectedIndexPath = IndexPath(item: 0, section: 0)
+    let randomTimeIndexPath = IndexPath(item: 3, section: 0)
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        if lastSelectedIndexPath != randomTimeIndexPath {
+            timePicker.isHidden = true
+        }
+
+        tableView.selectRow(at: lastSelectedIndexPath, animated: true, scrollPosition: UITableViewScrollPosition.none)
+        tableView.cellForRow(at: lastSelectedIndexPath)?.accessoryType = .checkmark
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = .checkmark
+        }
+
+        lastSelectedIndexPath = indexPath
+
+        if randomTimeIndexPath == indexPath {
+            timePicker.isHidden = false
+        }
+
+    }
+
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            cell.accessoryType = .none
+        }
+
+        if randomTimeIndexPath == indexPath {
+            timePicker.isHidden = true
+        }
+
+    }
+
 }
