0a0c4a2c02d56c5cbab7b1cc3cc3d93bfd59f3a0
Horváth Balázs <balazs630@icloud.com>
2017-12-07 13:31:11 +0100

    Add FMDB dependency with CocoaPods

---
+++ Podfile
@@ -0,0 +1,10 @@
+
+platform :ios, '10.0'
+
+target 'BadJokes' do
+  use_frameworks!
+
+  # Pods for BadJokes
+  pod 'FMDB'
+
+end
---
+++ Pods/Target Support Files/Pods-BadJokes/Pods-BadJokes-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BadJokes : NSObject
+@end
+@implementation PodsDummy_Pods_BadJokes
+@end
