04732bb53e2112edc06f2d3be6d1c1fc13763cea
Horváth Balázs <balazs630@icloud.com>
2018-05-17 01:37:31 +0200

    Create UIView factory extensions

---
+++ BadJokes/Extensions/UIViewExtensions.swift
@@ -0,0 +1,26 @@
+//
+//  UIViewExtensions.swift
+//  BadJokes
+//
+//  Created by Horváth Balázs on 2018. 05. 17..
+//  Copyright © 2018. Horváth Balázs. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    // swiftlint:disable force_cast
+
+    static func makeNoNotificationScheduledView() -> UIView {
+        return Bundle.main.loadNibNamed(UIView.noNotificationScheduledView,
+                                        owner: nil,
+                                        options: nil)!.first as! UIView
+    }
+
+    static func makeWaitingForFirstNotificationView() -> UIView {
+        return Bundle.main.loadNibNamed(UIView.waitingForFirstNotificationView,
+                                               owner: nil,
+                                               options: nil)?.first as! UIView
+    }
+
+}
