389c0aa59e8734837abc247e6486e1e715b277d2
Horváth Balázs <balazs630@icloud.com>
2019-08-20 00:36:53 +0200

    Add more jokes, delete duplicates

--- BadJokes/Storage/Queries/Migration Scripts/v1.4.sql
+++ BadJokes/Storage/Queries/Migration Scripts/v1.4.sql
@@ -1,1 +1,153 @@
 -- Database changes for app version: 1.4
+
+-- Delete duplicate jokes
+DELETE FROM jokes WHERE jokeId = CASE
+    WHEN (SELECT COUNT(*) FROM jokes WHERE jokeId = 162 OR jokeId = 195 AND isUsed = 0) = 2 THEN 195
+    WHEN (SELECT COUNT(*) FROM jokes WHERE jokeId = 162 OR jokeId = 195 AND isUsed = 1) = 2 THEN 195
+    WHEN jokeId = 162 OR jokeId = 195 AND isUsed = 0 THEN jokeId
+END;
+
+DELETE FROM jokes WHERE jokeId = CASE
+    WHEN (SELECT COUNT(*) FROM jokes WHERE jokeId = 147 OR jokeId = 270 AND isUsed = 0) = 2 THEN 270
+    WHEN (SELECT COUNT(*) FROM jokes WHERE jokeId = 147 OR jokeId = 270 AND isUsed = 1) = 2 THEN 270
+    WHEN jokeId = 147 OR jokeId = 270 AND isUsed = 0 THEN jokeId
+END;
+
+-- Add more jokes to the collection
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','- Mi lesz a műfogsorból ha alámegy a pirospaprika?\n- ???\n- Csípőprotézis.');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','- Most nem szabad a kávéfőzőnek titkokat elárulni?\n- ???\n- Mert kikotyogja.');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','- Mi lesz a túróból akit bolti lopáson kapnak?\n- ???\n- Körözött.');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'rough','- Mi van Monica Belucci hangpostáján?\n- ???\n- A szám jelenleg foglalt.');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','- Mi a baj fiacskám?\n- Jaj, az anyám beleesett a pöcegödörbe!\n- Te jó ég! - kiált fel a férfi majd elszántan a pöcegödörbe ugrik segíteni:\n- Fiam, sehol se találom az anyádat!\n A gyerek belenyúl a zsebébe, kiemel egy csavart és bedobja a gödörbe:\n- Akkor a csavar se kell!');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'animal','Versenyt repül a gém meg a gólya. Egy vadász észreveszi őket, előkapja a puskáját, lő. A gém élettelenól lezuhan. A gólya utánanéz:\n- Gém over...');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'anti','- Régen ismeri már ezt a pszichiátert?\n- Meglehetősen régen.\n- Maga is mániás depresszióval jár hozzá?\n- Nem. Én autóbusszal...');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'anti','Mi az: se keze se lába, és mégis minden este borozik a kocsmában?\n- ???\n- Törzsvendég.');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'anti','Mivel lehet leküzdeni a farkaséhséget?\n- ???\n- Piroskával.');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','Mondja hölgyem, a maga férje nem beszél amikor egyedül van?\n- Nem tudom. Még nem voltunk soha együtt amikor egyedül volt.');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'rough','Mit mond a szénégető, miután elhasal a hamuban?\n- ???\n- Hogy lehettem ilyen faszén...! ');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'moricka','Móricka bemegy a pékségbe és kér egy kiflit. Olyan kemény mint a kő.\n- Mai nincs?\n- De... Holnap lesz!');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'moricka','Mi szeretett volna lenni Móricka kiskorában?\n- Először asztal akart lenni, de széklet. Aztán meg komp akart lenni, de nem komplett.');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'moricka','Móricka, menj el szépen a henteshez és hozzál tőle hurkát.\n- Hurkáááát?\n- Igen, fiacskám, jól jegyezd meg: hurkát. De ne véreset.\n- Denevéreseeet?');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'moricka','Milyen időben vannak ezek az igék: én fázom, te fázol, ő fázik.\nMóricka válaszol:\n- Hideg időben tanárnő...');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'moricka','- Miért sírsz Móricka?\n- Mert a papám ráütött az ujjára kalapáccsal.\n- De hát ezért nem neked kéne sírnod!\n- Tudom, először én is röhögtem.');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'moricka','- Moricka, mondjál egy szót amiben két R betű van!\n- Tojás!\n- Na de Móricka, hol vannak az R betűk?\n- Egy a fehéRjében, másik a sáRgájában!');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'anti','Bocsánat uram, nem találkoztunk mi tavaly nyáron Pesten?\n- Nem, nem jártam ott tavaly.\n-Én sem. Akkor biztosan két másik ember volt.');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'animal','Két szú beszélget:\n- Mondja kolléga, miért nem jön át ebbe a remek bükkbe?\n- Mert a feleségem cserbenhagyott.');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'animal','Bemegy a patkány az elektronikai szaküzletbe.\n- Kérek egy tévét.\n- Színeset vagy fekete-fehéret?\n- Mindegy, csak sok csatorna legyen benne!');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','Mit énekelnek a molyok a szekrényben?\n- ???\n- Edda-búzt.');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'animal','Melyik az az állat, amelyiknek több mint a fele zsír?\n- ???\n- A zsiráf.');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'animal','Melyik a legsavanyúbb madár?\n- ???\n- A citromhéja!');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'animal','Miért szeretne a rókalány lövészversenyre menni?\n- ???\n- Mert ott mindig meghúzzák a ravaszt.');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','Mi villan át a légy agyán, amikor nekivágódik a szélvédőnek?\n- ???\n- A hátsója...');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'anti','Miért nincs a méhecskének 45-ös lába?\n- ???\n- Mert letaposná a  virágokat.');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','Hogy hívják a jóllakott szúnyogot?\n- ???\n- Telivér.');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'anti','Mit tettek az ősmagyarok a nyereg alá?\n- ???\n- Lovat.');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'jean','- Mondja Jean, szőrös a csengő?\n- Nem uram.\n- Akkor az éjjel megint a házmesternét nyomtam meg.');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'jean','- Itt vannak a fagyosszentek, uram.\n- Jól van Jean, akkor engedje be őket!');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'jean','- Jean, vegyen elő egy ruhaakasztót a szekrényből, kirándulni megyünk.\n- És minek ahhoz ruhaakasztó, uram?\n- Hogy fogassal menjünk a Szabadság-hegyre.');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'jean','- Jean, mi az ott a fa tetején?\n- ???\n- A fészkes fene, uram.');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'jean','- Jean, mondja mi után kutat a kamrában?\n- ???\n- Kenyérkereső vagyok, uram.');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'jean','- Jean, mi ez a trappolás az udvaron?\n- ???\n- Edzenek a futórózsák, uram.');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'jean','- Jean, mik ezek a kék foltok a testén?\n- ???\n- Cseberből vederbe estem, uram.');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'jean','- Mivel locsoljam meg a virágokat, uram?\n- ???\n- Mivel szárazak, Jean...');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'moricka','Morickáék nyaralnak a vízparton: Anyu, én meghaltam?\n- Miért kérdezel ilyen hülyeséget kisfiam?\n- Ott jön a hullám...');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'jean','- Jean, tegye be az ajtót!\n- Hova uram?');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','- Hogy hívják a sivatagi anyóst?\n- ???\n- Tevén banya.');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
+INSERT INTO jokes (isUsed, deliveryTime, type, jokeText) VALUES (0,null,'tiring','');
